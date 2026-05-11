d14dabfdf7c710167f416f3351c4116af49f9192
Sapfire151 <yell0wcircle12345@gmail.com>
2026-05-11 15:29:55 +0700

    Update and rename config.local.example.js to config.local.js

---
+++ public/config.local.js
@@ -0,0 +1,9 @@
+window.__TCASX_FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyDP18fvle5Ls1mPPd6OVHII7Ay2_thaHbQ",
+  authDomain: "tcasx-48020.firebaseapp.com",
+  projectId: "tcasx-48020",
+  storageBucket: "tcasx-48020.firebasestorage.app",
+  messagingSenderId: "782302455229",
+  appId: "1:782302455229:web:5655f95a226e0015e59ed4",
+  measurementId: "G-JXR0PHP08E"
+};
